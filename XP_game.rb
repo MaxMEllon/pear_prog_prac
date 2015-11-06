437ff3f4da33c6960fb47f92c090400cc59718e8
MaxMEllon <mozi_kke@excite.co.jp>
2015-11-06 18:17:02 +0900

    create tax function

--- XP_game.rb
+++ XP_game.rb
@@ -1,8 +1,19 @@
-puts "値段を入力してください"
-price =  gets.chomp.to_i
-puts "個数を入力してください"
-count = gets.chomp.to_i
-puts "場所を入力してください"
-prace = gets.chomp
 
-print price, " ", count, " ", prace,"\n"
+def imput_data
+  puts "値段を入力してください"
+  @price =  gets.chomp.to_i
+  puts "個数を入力してください"
+  @count = gets.chomp.to_i
+  puts "場所を入力してください"
+  @prace = gets.chomp
+end
+
+imput_data
+
+def tax(tax_rate)
+  tmp = tax_rate/100
+  tmp += 1.0
+  @price *= tmp
+end
+
+print @price, " ", @count, " ", @prace,"\n"
