dadaeab8d4631794688a96792e7ddd2b82694c54
MaxMEllon <mozi_kke@excite.co.jp>
2015-11-06 18:25:16 +0900

    add areas

--- XP_game.rb
+++ XP_game.rb
@@ -1,4 +1,6 @@
 
+@areas = {'hokkaidou': 0.0685, 'higashi_nihon': 0.08, 'nishi_nihon': 0.0625, 'shikoku': 0.04, 'kyusyu': 0.0825}
+
 def imput_data
   puts "値段を入力してください"
   @price =  gets.chomp.to_i
@@ -8,12 +10,13 @@ def imput_data
   @prace = gets.chomp
 end
 
-imput_data
-
 def tax(tax_rate)
-  tmp = tax_rate/100
-  tmp += 1.0
+  tmp = 1 + tax_rate
+  @price *= @count
   @price *= tmp
 end
+
+imput_data
+tax(0.05)
 
 print @price, " ", @count, " ", @prace,"\n"
